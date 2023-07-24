830ac4bacb5e02e4f924e6473db9d820925cc85e
FR1998 <iamfaizan199@gmail.com>
2023-07-24 19:04:56 +0500

    view added

--- product_catalogue/views.py
+++ product_catalogue/views.py
@@ -1,49 +1,51 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from product_catalogue.models import *
-import json
-# Create your views here.
+
 
 def display_product(request):
-    with open('product_catalogue/data/clothes.json') as f:
-        data = json.load(f)
-        
-        for item in data:
-            for category in item["product_info"]:
-                category = Category(name=category)
-                category.save()
-                
-            product = Product(
-                name=item["product_name"],
-                price=item["product_price"],
-                code=item["product_code"]
-            )
-            product.save()
-            
-            for image_url in item["product_images"]:
-                images = ProductImage(product=product, image=image_url)
-                images.save()
-                
-            for one_feature in item["product_info"]:
-                features = ProductFeature(product=product, feature=one_feature)
-                features.save()    
-                 
     products = Product.objects.all()
-    context = {"products": products}    
+    category = Category.objects.all()
+    
+    active_products = Paginator(products,8)
+    page = request.GET.get("page")
+    active_page = active_products.get_page(page)
+    
+    context = {"products": active_page, "categories": category}
+        
     return render(request, "display_product.html", context)
+
+
+def display_with_category(request,category_id):
+    sort_by = request.GET.get("sort", "name")
+    category = Category.objects.get(id=category_id) 
+    products = Product.objects.filter(category=category).order_by(sort_by) 
+    
+    active_products = Paginator(products,6)
+    page = request.GET.get("page")
+    active_page = active_products.get_page(page)
+    
+    context = {"products": active_page}      
+    
+    return render(request, "display_category_wise.html", context)
+    
 
 def display_detail_product(request, id):
     product = Product.objects.filter(id=id)
     context = {"products": product}    
+    
     return render(request, "display_detail_product.html", context)
 
-# def display_with_category(request,category):
-#     # products = Product.objects.filter(product_features__category__category_name__icontains=category)
-#     products = Product.objects.all()
-#     context = {"products": products}    
-#     return render(request, "display_category_wise.html", context)
+
+def search_products(request):
+    query_name = request.GET.get("query", None)
+    products = Product.objects.filter(name__icontains=query_name)
     
+    active_products = Paginator(products,12)
+    page = request.GET.get("page")
+    active_page = active_products.get_page(page)           # I will try to dry this pagination code tomorrow
+    
+    context = {"products": active_page} 
+    
+    return render(request, "search_product.html", context)
 
-
-#products.product_features_set.all() to access all features
-#Category.objects.filter(category_name="Cotton").values() by id 
-        
