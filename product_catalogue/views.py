52bc05301a99cbfb00fedfd72cecfef75fc6040d
FR1998 <iamfaizan199@gmail.com>
2023-07-25 16:58:54 +0500

    product feature

--- product_catalogue/views.py
+++ product_catalogue/views.py
@@ -1,51 +1,45 @@
 from django.shortcuts import render
-from django.core.paginator import Paginator
 from product_catalogue.models import *
+from product_catalogue.pagination import get_pagination
 
 
-def display_product(request):
+def product_listing(request):
     products = Product.objects.all()
     category = Category.objects.all()
     
-    active_products = Paginator(products,8)
-    page = request.GET.get("page")
-    active_page = active_products.get_page(page)
-    
+    active_page = get_pagination(request, products)
+
     context = {"products": active_page, "categories": category}
         
-    return render(request, "display_product.html", context)
+    return render(request, "index.html", context)
 
 
-def display_with_category(request,category_id):
+def list_products_with_category(request, category_id):
     sort_by = request.GET.get("sort", "name")
     category = Category.objects.get(id=category_id) 
     products = Product.objects.filter(category=category).order_by(sort_by) 
     
-    active_products = Paginator(products,6)
-    page = request.GET.get("page")
-    active_page = active_products.get_page(page)
+    active_page = get_pagination(request, products)
     
-    context = {"products": active_page}      
+    context = {"products": active_page, "sorting":sort_by}      
     
-    return render(request, "display_category_wise.html", context)
+    return render(request, "listing_with_category.html", context)
     
 
-def display_detail_product(request, id):
+def get_product_details(request, id):
     product = Product.objects.filter(id=id)
     context = {"products": product}    
     
-    return render(request, "display_detail_product.html", context)
+    return render(request, "details.html", context)
 
 
 def search_products(request):
     query_name = request.GET.get("query", None)
     products = Product.objects.filter(name__icontains=query_name)
     
-    active_products = Paginator(products,12)
-    page = request.GET.get("page")
-    active_page = active_products.get_page(page)           # I will try to dry this pagination code tomorrow
+    active_page = get_pagination(request,products)    
     
-    context = {"products": active_page} 
+    context = {"products": active_page, "query":query_name} 
     
     return render(request, "search_product.html", context)
 
