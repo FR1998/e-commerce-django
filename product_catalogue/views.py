31f9dbef6b28b7babbdbf88b486e91b9d5eb7ea8
FR1998 <iamfaizan199@gmail.com>
2023-07-25 17:14:28 +0500

    product_catalogue views and templates

--- product_catalogue/views.py
+++ product_catalogue/views.py
@@ -9,7 +9,7 @@ def product_listing(request):
     
     active_page = get_pagination(request, products)
 
-    context = {"products": active_page, "categories": category}
+    context = {"products":active_page, "categories":category}
         
     return render(request, "index.html", context)
 
@@ -21,14 +21,14 @@ def list_products_with_category(request, category_id):
     
     active_page = get_pagination(request, products)
     
-    context = {"products": active_page, "sorting":sort_by}      
+    context = {"products":active_page, "sorting":sort_by}      
     
     return render(request, "listing_with_category.html", context)
     
 
 def get_product_details(request, id):
     product = Product.objects.filter(id=id)
-    context = {"products": product}    
+    context = {"products":product}    
     
     return render(request, "details.html", context)
 
@@ -37,9 +37,9 @@ def search_products(request):
     query_name = request.GET.get("query", None)
     products = Product.objects.filter(name__icontains=query_name)
     
-    active_page = get_pagination(request,products)    
+    active_page = get_pagination(request, products)    
     
-    context = {"products": active_page, "query":query_name} 
+    context = {"products":active_page, "query":query_name} 
     
     return render(request, "search_product.html", context)
 
