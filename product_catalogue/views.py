60689a422668885536e96c1524de5f8c85ba9cbd
FR1998 <iamfaizan199@gmail.com>
2023-07-26 13:10:04 +0500

    Class-based-views

--- product_catalogue/views.py
+++ product_catalogue/views.py
@@ -1,45 +1,48 @@
 from django.shortcuts import render
+from django.views import View
 from product_catalogue.models import *
 from product_catalogue.pagination import get_pagination
 
 
-def product_listing(request):
-    products = Product.objects.all()
-    categories = Category.objects.all()
+class ProductView(View):
+    def get(self, request):
+        products = Product.objects.all()
+        categories = Category.objects.all()
     
-    active_page = get_pagination(request, products)
+        active_page = get_pagination(request, products)
 
-    context = {"products":active_page, "categories":categories}
+        context = {"products":active_page, "categories":categories}
         
-    return render(request, "index.html", context)
-
-
-def list_products_with_category(request, category_id):
-    sort_by = request.GET.get("sort", "name")
-    category = Category.objects.get(id=category_id) 
-    products = Product.objects.filter(category=category).order_by(sort_by) 
-    
-    active_page = get_pagination(request, products)
-    
-    context = {"products":active_page, "sorting":sort_by}      
-    
-    return render(request, "listing_with_category.html", context)
+        return render(request, "index.html", context)
     
 
-def get_product_details(request, id):
-    product = Product.objects.get(id=id)
-    context = {"product":product}    
+class CategoryProductsView(View):
+    def get(self, request, category_id):
+        sort_by = request.GET.get("sort", "name")
+        category = Category.objects.get(id=category_id) 
+        products = Product.objects.filter(category=category).order_by(sort_by) 
     
-    return render(request, "details.html", context)
+        active_page = get_pagination(request, products)
+    
+        context = {"products":active_page, "sorting":sort_by}      
+    
+        return render(request, "listing_with_category.html", context)
+    
+    
+class ProductDetailView(View):
+    def get(self, request, id):
+        product = Product.objects.get(id=id)
+        context = {"product":product}    
+    
+        return render(request, "details.html", context)
 
 
-def search_products(request):
-    query_name = request.GET.get("query", None)
-    products = Product.objects.filter(name__icontains=query_name)
+class SearchProductsView(View):
+    def get(self, request):
+        query_name = request.GET.get("query", None)
+        products = Product.objects.filter(name__icontains=query_name)
+        
+        context = {"products": products, "query": query_name}
+        
+        return render(request, "search_product.html", context)
     
-    active_page = get_pagination(request, products)    
-    
-    context = {"products":active_page, "query":query_name} 
-    
-    return render(request, "search_product.html", context)
-
