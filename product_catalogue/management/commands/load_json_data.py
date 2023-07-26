bd1579d3629c145b47c9d82902f9acd452c7a168
FR1998 <iamfaizan199@gmail.com>
2023-07-26 11:00:49 +0500

    changes

--- product_catalogue/management/commands/load_json_data.py
+++ product_catalogue/management/commands/load_json_data.py
@@ -7,43 +7,37 @@ from product_catalogue.models import *
 
 
 class Command(BaseCommand):
-     def handle(self):
+    def handle(self, *args, **options):
         with open('product_catalogue/data/clothes.json') as product_readings:
-            product_values = json.load(product_readings)
+            product_detail = json.load(product_readings)
 
-            for item in product_values:
-                product_info = item.get("product_info", {})
-                category_name = product_info.get("Product Category", "Product does not have a category")
+            for product_attributes in product_detail:
+                product_specifications = product_attributes.get("product_info", {})
+                category_name = product_specifications.get("Product Category", "Product does not have a category")
                 
-                categories = Category.objects.filter(name=category_name)
+                category = Category.objects.get_or_create(name=category_name)
 
-                if categories.exists():
-                    category = categories.first() 
-                else:
-                    category = Category(name=category_name)
-                    category.save()
+                product_price = product_attributes["product_price"]
+                product_price = Decimal(re.sub(r'[^\d.]', '', product_price))
 
-                product_price = item.get("product_price", "")
-            
-                if isinstance(product_price, str):
-                    product_price = Decimal(re.sub(r'[^\d.]', '', product_price))
-
-                product = Product(
-                    name=item["product_name"],
+                product = Product.objects.create(
+                    name=product_attributes["product_name"],
                     price=product_price,
-                    code=item["product_code"],
-                    category=category
+                    code=product_attributes["product_code"],
+                    category=category 
                 )
                 
+                product.category = category
                 product.save()
-                product.product_info = {category.name: product_info.get("Product Category")}
+                
+                product.product_specifications = {category.name: product_specifications.get("Product Category")}
                 product.save()
 
-                for image_urls in item["product_images"]:
-                    image_url = ProductImage(product=product, image=image_urls)
-                    image_url.save()
+                for image_url in product_attributes["product_images"]:
+                    image = ProductImage(product=product, image=image_url)
+                    image.save()
 
-                for one_feature_key, one_feature_value in product_info.items():
-                    feature = ProductFeature(product=product, name=one_feature_key, value=one_feature_value)
+                for name, value in product_specifications.items():
+                    feature = ProductFeature(product=product, name=name, value=value)
                     feature.save()
-        
+
